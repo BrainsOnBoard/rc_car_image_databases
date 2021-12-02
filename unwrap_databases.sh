d3abbbb3c5ec05707dbe1706a9ff218eaa985686
Alex Dewar <a.dewar@sussex.ac.uk>
2021-12-02 15:13:50 +0000

    Fix unwrap_databases.sh

--- unwrap_databases.sh
+++ unwrap_databases.sh
@@ -2,4 +2,4 @@
 set -e
 
 # Requires that image_database_unwrapper (from bob_robotics/tools) is on PATH
-find . -type d -name 'dataset*' -exec image_database_unwrapper {} 720 150 \;
+find . -type d -name 'dataset*' -exec image_database_unwrapper {} -r 720 150 \;
